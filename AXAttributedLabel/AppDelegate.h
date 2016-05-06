b4ff0666f3695de2698d4d050c27a97b3c3727e6
aiXing <862099730@qq.com>
2016-05-06 11:26:12 +0800

    Initial Commit

---
+++ AXAttributedLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXAttributedLabel
+//
+//  Created by ai on 16/5/6.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AXAttributedLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXAttributedLabel
+//
+//  Created by ai on 16/5/6.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
